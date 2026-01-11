b23f4b3a67f2492455b666f25f6db924db59565d
Aasthap3 <porwalaastha@gmail.com>
2026-01-12 00:48:06 +0530

    [A11] Fetched service categories on home page

--- server/src/routes/publicRoutes.js
+++ server/src/routes/publicRoutes.js
@@ -6,6 +6,7 @@ import {
   PostFeedback,
   getAllFeedback,
   getFeedbackByserviceId,
+  getPublicCategories,
 } from "../controllers/publicController.js";
 
 const router = express.Router();
@@ -16,5 +17,6 @@ router.get("/track", TrackService);
 router.post("/feedback", PostFeedback);
 router.get("/feedback", getAllFeedback);
 router.get("/feedback/:id", getFeedbackByserviceId);
+router.get("/categories", getPublicCategories);
 
 export default router;
