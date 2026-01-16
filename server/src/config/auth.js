cf9bdcaab6651d7507ea22b34bd0076faccb27be
Raj Vardhan <errvmaurya563@gmail.com>
2026-01-16 14:02:11 +0530

    Update SameSite attribute for cookies based on environment

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -9,7 +9,7 @@ export const genUserToken = async (user, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
   } catch (error) {
