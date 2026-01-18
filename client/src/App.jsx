18b9b6bc93887dd9caa72f61fe60e847a0dd3c91
Raj Vardhan <errvmaurya563@gmail.com>
2026-01-18 19:24:30 +0530

    toast timeout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,30 @@ const Layout = () => {
 
   return (
     <>
-      <Toaster />
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={8}
+        toastOptions={{
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 5000,
+            theme: {
+              primary: '#4aed88',
+            },
+          },
+          error: {
+            duration: 5000,
+            theme: {
+              primary: '#ff4b4b',
+            },
+          },
+        }}
+      />
       <TopHeader />
       <Header />
 
